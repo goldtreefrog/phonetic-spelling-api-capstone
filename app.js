c330cdc65eb7ed981a0091a304328479f97123a1
Margaret Blauvelt <margaret@blauvelt.com>
2017-12-08 08:35:44 -0800

    Reorganize JS code.

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 'use strict';
 
-// Function: main
-// Carries out initial tasks.
-function main() {
-  let zoomFactor = 1;
-  handleClicks(zoomFactor);
+// Step 1: Defining global variables, functions and objects
+
+let zoomFactor = 1;
+
+function voiceStartCallback(userMsg) {
+    console.log("Voice started");
+    if(confirm(userMsg)) {
+      $('textarea').val('');
+    }
 }
 
-// Function: handleClicks
-// Click listeners: Respond to user clicks of buttons and links (other than straight HTML navigation).
-function handleClicks (zFactor) {
+// Function: zoomPage
+// Zoom function
+function zoomPage(zoomFactor, step) {
+  // Maximum zoom in or out is double or half; when exceeded, return to default browser size.
+  zoomFactor += step;
+  if(zoomFactor > 2.1 || zoomFactor < .5) {
+    zoomFactor = 1;
+  }
+  $('body').css('zoom', `${zoomFactor*100}%`)
+  return zoomFactor;
+}
 
+
+// Step 2: Using global variables, functions and objects (document ready and triggers)
+
+// document ready goes
+$(document).ready(function() {
+
+// Zoom in
   $('#plus').on('click', function(e) {
     e.preventDefault();
     let step = .1
-    zFactor = zoomPage(zFactor, step);
+    zoomFactor = zoomPage(zoomFactor, step);
   })
 
+// Zoom out
   $('#minus').on('click', function(e) {
     e.preventDefault();
     let step = -.1
-    zFactor = zoomPage(zFactor, step);
+    zoomFactor = zoomPage(zoomFactor, step);
   })
 
+// Read text aloud
   $('#read-aloud').on('click', function(e) {
     e.preventDefault();
     let textToSpeak = $.trim($('textarea').val());
@@ -35,38 +56,18 @@ function handleClicks (zFactor) {
     responsiveVoice.speak(textToSpeak);
   })
 
+// Delete text in textarea
   $('#clear').on('click', function(e) {
     e.preventDefault();
 
     let userMsg = "Click OK to delete your text or Cancel to keep it there."
 
     var rVParameters = {
-        onstart: voiceStartCallback,
+        onstart: voiceStartCallback(userMsg), 
         // onend: voiceEndCallback
-    }
-    function voiceStartCallback() {
-        console.log("Voice started");
-        if(confirm(userMsg)) {
-          $('textarea').val('');
-        }
     }
 
     responsiveVoice.speak(userMsg,"US English Male", rVParameters);
 
   });
-
-}
-
-// Function: zoomPage
-// Zoom function
-function zoomPage(zFactor, step) {
-  // Maximum zoom in or out is double or half; when exceeded, return to default browser size.
-  zFactor += step;
-  if(zFactor > 2.1 || zFactor < .5) {
-    zFactor = 1;
-  }
-  $('body').css('zoom', `${zFactor*100}%`)
-  return zFactor;
-}
-
-$(main());
+});
