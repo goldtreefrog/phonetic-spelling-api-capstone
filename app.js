2f17d5dda4a6f2f0b469454e115b7db64ee3f6f7
Margaret Blauvelt <margaret@blauvelt.com>
2017-12-12 16:43:19 -0800

    Pass words to Datamuse API that were identified as misspelled by LanguageTool API. (Not finished.)
    Change More link to a button so I can store JS index for misspelled words array.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Step 1: Defining global variables, functions and objects
 
@@ -16,10 +16,10 @@ let zoomPercent = 100;
 function zoomPage(zoomPercent, step) {
   // Maximum zoom in or out is double or half; when exceeded, return to default browser size.
   zoomPercent += step;
-  if(zoomPercent > 201 || zoomPercent < 50) {
+  if (zoomPercent > 201 || zoomPercent < 50) {
     zoomPercent = 100;
   }
-  $('body').css('zoom', `${zoomPercent}%`)
+  $("body").css("zoom", `${zoomPercent}%`);
   return zoomPercent;
 }
 
@@ -27,74 +27,78 @@ function zoomPage(zoomPercent, step) {
 function sendToLanguageToolAPI(textToCheck) {
   // Find misspellings with LanguageTool
   var params = {
-      site: '',
-      text: textToCheck,
-      language: "en-us"
+    site: "",
+    text: textToCheck,
+    language: "en-us"
   };
 
-  let url = "https://languagetool.org/api/v2/check"
+  let url = "https://languagetool.org/api/v2/check";
 
   $.getJSON(url, params, parseLTResults);
 }
 
 function parseLTResults(data) {
-  // data.forEach(function(mistake){
-  //   console.log(mistake);
-  // });
-  // console.log(data.matches);
-  // console.log(data.matches[0].length);
-  // console.log(data.matches[0].offset);
-  // // shortMessage:"Spelling mistake"
-  // console.log(data.matches[0].shortMessage);
-  // console.log(data.matches[0].sentence);
-  // var res = data.matches[0].sentence.substring(data.matches[0].offset, data.matches[0].length);
-  // console.log(res);
+  const misspellings = [];
 
-  $.each(data.matches, function (dataKey, dataValue) {
-    console.log(dataValue, dataKey);
+  $.each(data.matches, function(dataKey, dataValue) {
+    // console.log("dataValue, dataKey");
+    // console.log(dataValue, dataKey);
     let offs = dataValue.offset;
+    // console.log("offset");
     // console.log(dataValue.offset);
     let len = dataValue.length;
+    // console.log("length");
     // console.log(dataValue.length);
     // console.log(dataValue.shortMessage);
     let sent = dataValue.sentence;
-    // console.log(res);
     // console.log(dataValue.sentence);
-    var res = sent.substring(offs, (len + offs));
-    console.log(res);
-    // Search for alternatives to phonetic spelling
+    var res = sent.substring(offs, len + offs);
+    // console.log(res);
 
+    // Push misspelled word and its offset into the misspellings array
+    if (dataValue.shortMessage === "Spelling mistake") {
+      misspellings.push({ mWord: res, offset: offs });
+    }
   });
+  // Search for alternatives to phonetic misspelling
+  if (misspellings.length > 0) {
+    offerSpellings(misspellings, 0);
+  }
+}
 
+function offerSpellings(misspellings, curRow) {
+  $("button#correct").val([misspellings[curRow].offset, curRow]);
+  getSpellingFromAPI(misspellings[curRow].mWord);
 }
 
 // Function: getSpellingFromAPI
 // Check spelling of word. If incorrect, return suggestions
-function getSpellingFromAPI (wordToCheck) {
-  // Right now we send ALL words to Datamuse. We only want incorrect ones! *** FIX THIS ****
+function getSpellingFromAPI(wordToCheck) {
   console.log("2. In getSpellingFromAPI");
-  let url = "https://api.datamuse.com/words?sl=spageti"
+  console.log("wordToCheck: ", wordToCheck);
+  let url = "https://api.datamuse.com/words";
 
   var params = {
-      site: ''     // Keeps browsers happy, though API doesn't require it.
-      // max: 20,      // Default is 100.
-      // sl: wordToCheck
+    site: "", // Keeps browsers happy, though API doesn't require it.
+    max: 20, // Default is 100.
+    sl: wordToCheck
   };
 
-  $.getJSON(url, params, suggestSpelling)  // FIX THIS add callback
+  $.getJSON(url, params, suggestSpelling); // FIX THIS add callback
 }
 
 // Function: suggestSpelling
 // Display suggested spellings for misspelled word
 function suggestSpelling(data) {
   console.log("3. In suggestSpelling");
- //  console.log(data[0]);
- // $("#spelling-choices").prepend("<li>" + data[0].word + "</li>")
+  console.log(data);
+  // if // Here we want to check counter for displayed words by looking at value of More button.
+  // $("#spelling-choices").prepend("<li>" + data[0].word + "</li>")
 }
 
 function sayIt(textToSay, focusElem) {
   responsiveVoice.speak(textToSay);
-  if(focusElem) {
+  if (focusElem) {
     $(focusElem).focus();
   }
 }
@@ -103,80 +107,83 @@ function sayIt(textToSay, focusElem) {
 
 // document ready; listen for triggers
 $(document).ready(function() {
-
-// Zoom in
-  $('#plus').on('click', function(e) {
+  // Zoom in
+  $("#plus").on("click", function(e) {
     e.preventDefault();
-    let step = 10
+    let step = 10;
     zoomPercent = zoomPage(zoomPercent, step);
-  })
+  });
 
-// Zoom out
-  $('#minus').on('click', function(e) {
+  // Zoom out
+  $("#minus").on("click", function(e) {
     e.preventDefault();
-    let step = -10
+    let step = -10;
     zoomPercent = zoomPage(zoomPercent, step);
-  })
+  });
 
-// Read text aloud using Responsive Voice API
+  // Read text aloud using Responsive Voice API
 
-  $('#read-aloud').on('click', function(e) {
+  $("#read-aloud").on("click", function(e) {
     e.preventDefault();
-    let textToSpeak = $.trim($('textarea').val());
+    let textToSpeak = $.trim($("textarea").val());
 
     if (!textToSpeak) {
-      sayIt("Type in the big box and click the Read Aloud button again.", "textarea" )
+      sayIt(
+        "Type in the big box and click the Read Aloud button again.",
+        "textarea"
+      );
       // return;
     } else {
-      sayIt(textToSpeak)
+      sayIt(textToSpeak);
       // responsiveVoice.speak(textToSpeak);
     }
-  })
+  });
 
-// Identify misspelling using LanguageTool API
-  $('#misspellings').on('click', function(e) {
+  // Identify misspelling using LanguageTool API
+  $("#misspellings").on("click", function(e) {
     e.preventDefault();
-    console.log('1. Check Spelling clicked');
+    console.log("1. Check Spelling clicked");
 
     // See if there is any text at all.
-    let textToSpell = $.trim($('textarea').val());
+    let textToSpell = $.trim($("textarea").val());
 
     if (!textToSpell) {
-      sayIt("Type in the big box and click the Check Spell ing button again.", "textarea")
+      sayIt(
+        "Type in the big box and click the Check Spell ing button again.",
+        "textarea"
+      );
       return;
     }
 
     sendToLanguageToolAPI(textToSpell);
-
   });
 
-// Delete text in textarea
-  $('#clear').on('click', function(e) {
+  // Delete text in textarea
+  $("#clear").on("click", function(e) {
     e.preventDefault();
 
-    if(!$('textarea').val()) {
-      sayIt("The big box is already empty.", "textarea")
+    if (!$("textarea").val()) {
+      sayIt("The big box is already empty.", "textarea");
       return;
     }
 
-    let userMsg = "Click OK to delete your text or Cancel to keep it there."
+    let userMsg = "Click OK to delete your text or Cancel to keep it there.";
 
     var parameters = {
-        onstart: voiceStartCallback
-        // onend: voiceStartCallback(userMsg) // Does not work
-    }
+      onstart: voiceStartCallback
+      // onend: voiceStartCallback(userMsg) // Does not work
+    };
 
-    responsiveVoice.speak(userMsg,"US English Male", parameters);
+    responsiveVoice.speak(userMsg, "US English Male", parameters);
 
     // For some as-yet unknown reason, if you pass userMsg to the voiceStartCallBack function, the confirm dialog popsup and the Responsive Voice message is not spoken UNTIL AFTER the dialog box is closed. Inside the RV function, you do not have to pass the message and everything works as desired.
     function voiceStartCallback() {
-        console.log("Voice start");
-        if(confirm(userMsg)) {
-          $('textarea').val('');
-        }
+      console.log("Voice start");
+      if (confirm(userMsg)) {
+        $("textarea").val("");
+      }
     }
 
-    $('textarea').focus();
-
+    $("textarea").focus();
   });
 });
